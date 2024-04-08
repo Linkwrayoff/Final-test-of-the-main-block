037b00608ab4c403d1f78df8e52487d85dce60ba
Artyom <linkwrayoff@gmail.com>
2024-04-08 19:55:37 +0300

    problem solved with code

--- Program.cs
+++ Program.cs
@@ -1,1 +1,18 @@
 ﻿string[] arr = { "Hello", "2", "world", ":-)" };
+int j = 0;
+ 
+for(int index = 0; index < arr.Length; index++){
+	if(arr[index].Length <= 3){
+		j++;
+	}
+} 
+
+string [] arr2 = new string[j] ;
+j = 0;
+for(int index = 0; index < arr.Length; index++){
+	if(arr[index].Length <= 3){
+		arr2[j] = arr[index];
+		j++;
+	}
+} 
+Console.WriteLine(string.Join(", ", arr2));
